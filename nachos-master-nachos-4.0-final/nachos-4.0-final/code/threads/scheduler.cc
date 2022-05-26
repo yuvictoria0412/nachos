dfd16c002c5c7bb445a649ba20c2fd60257103df
yuvictoria0412 <yuvictoria0412@gmail.com>
2022-05-26 23:36:33 +0800

    debug

--- nachos-master-nachos-4.0-final/nachos-4.0-final/code/threads/scheduler.cc
+++ nachos-master-nachos-4.0-final/nachos-4.0-final/code/threads/scheduler.cc
@@ -92,16 +92,16 @@ Scheduler::ReadyToRun (Thread *thread)
     thread->setPredictedBurstTime(0.5 * kernel->currentThread->getT() + 0.5 * (PreviousBurstTime));
     kernel->scheduler->setPreviousBT(thread->getPredictedBurstTime());
 
-    // if (SJFcmp(thread, kernel->currentThread) < 0) {
-    //     kernel->scheduler->ReadyToRun(kernel->currentThread);
-    //     kernel->scheduler->Run(thread, FALSE);
-    // }
+    if (SJFcmp(thread, kernel->currentThread) < 0) {
+        kernel->scheduler->ReadyToRun(kernel->currentThread);
+        kernel->scheduler->Run(thread, FALSE);
+    }
     // victoria
-    // else {
+    else {
         DEBUG(dbgThread, "Putting thread on ready list: " << thread->getName());
         thread->setStatus(READY);
         readyQueue->Insert(thread);
-    // }
+    }
 }
 //<TODO>
 
